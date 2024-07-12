1f539647d34c0c25d3a7fd91aba49a684d662f0b
Rifqi Maulana <abelcaster025@gmail.com>
2024-07-12 23:00:54 +0700

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.valorant-api.com'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
